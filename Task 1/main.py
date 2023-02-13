3c4f6962fe8fe08cb61b7b4e9709b678c349c8c7
Май Ван Туан <86241573+8268826@users.noreply.github.com>
2023-02-13 08:27:40 +0300

    taks 1

--- Task 1/main.py
+++ Task 1/main.py
@@ -1,1 +1,9 @@
-print('tuan')
+import math
+
+
+def main(z, y, x):
+    return (((59 * (z ** 2) - 33 * (y ** 3) - 94 * y) ** 2) + x) / ((97 * (x ** 2) + y + (z ** 3)) ** 6) + math.sqrt(
+        (math.atan(z) ** 4) - (x / 33 - 3 * (y ** 3) - 96 * (x ** 2)) ** 3)
+
+
+print(main(-0.56, 0.66, 0.72))
