249977976ce7fe3c21ab06b071c4bf15a9c7132d
Май Ван Туан <86241573+8268826@users.noreply.github.com>
2023-02-16 21:12:27 +0300

    taks 5

--- Task 5/main.py
+++ Task 5/main.py
@@ -1,8 +1,7 @@
 def main(y, z):
     n = len(y) - 1
-    result = sum(((z[int(n + 1 - ((i / 2) // 1 + 1))]) ** 2 + y[int((i / 3) // 1 + 1)] + 58 * (y[int((i / 2) // 1 + 1)])
-                  ** 3) ** 4 for i in range(1, n + 1))
-    return result
+    return sum(((z[int(n + 1 - ((i / 2) // 1 + 1))]) ** 2 + y[int((i / 3) // 1 + 1)] + 58 * (y[int((i / 2) // 1 + 1)])
+                ** 3) ** 4 for i in range(1, n + 1))
 
 
 print(main([0.94, -0.01, 0.22, -0.06, 0.35, 0.45, -0.27], [-0.17, -0.3, 0.52, -0.1, 0.68, 0.08, 0.13]))
