dc5311c4d9ff39c5a604bc796aedb83acca29fef
Май Ван Туан <86241573+8268826@users.noreply.github.com>
2023-02-17 09:07:45 +0300

    taks 5

--- Task 5/main.py
+++ Task 5/main.py
@@ -4,4 +4,5 @@ import math
 def main(y, z):
     return sum(((z[len(y) - math.ceil(i / 2)])
                 ** 2 + y[math.ceil(i / 3) - 1] + 58 *
-                (y[math.ceil(i / 2) - 1]) ** 3) ** 4 for i in range(1, len(y) + 1))
+                (y[math.ceil(i / 2) - 1]) ** 3) ** 4
+               for i in range(1, len(y) + 1))
