d361758ff9adfa790d474597a3349ccdbfb6279f
Май Ван Туан <86241573+8268826@users.noreply.github.com>
2023-02-20 08:13:09 +0300

    prac

--- Task 6/main.py
+++ Task 6/main.py
@@ -1,10 +1,86 @@
 import math
+import tkinter as tk
 
 
-def main(y):
-    n = len(y) - 1
-    return 22 * sum(44 * math.atan(46 * y[int(n + 1 - i)]) ** 4 for i in range(1, len(y) - 1))
+def pyshader(func, w, h):
+    scr = bytearray((0, 0, 0) * w * h)
+    for y in range(h):
+        for x in range(w):
+            p = (w * y + x) * 3
+            scr[p:p + 3] = [max(min(int(c * 255), 255), 0)
+                            for c in func(x / w, y / h)]
+    return bytes('P6\n%d %d\n255\n' % (w, h), 'ascii') + scr
 
 
-print(main([0.64, -0.74, -0.72, -0.2, -0.77, 0.98, -0.18, 0.48]))
-print(2.92e+04)
+def noise(x, y):
+    # применяем хэш-функцию к координатам
+    h = hash((x, y))
+    # используем полученное число для создания шума
+    noise_val = (h % 10000) / 10000.0
+    # применяем к шуму некоторые преобразования для улучшения качества
+    noise_val = 0.5 + 0.5 * math.sin(noise_val * 2 * math.pi * 10)
+    return noise_val, noise_val, noise_val
+
+
+def val_noise(x, y, octaves=6, persistence=300, lacunarity=.05):
+    total = 0.0
+    frequency = 1.0
+    amplitude = 1.0
+    max_val = 0.0
+    for i in range(octaves):
+        val = noise(x * frequency, y * frequency)
+        total += val[0] * amplitude
+        max_val += amplitude
+        amplitude *= persistence
+        frequency *= lacunarity
+    return total / max_val, total / max_val, total / max_val
+
+
+# def fractal_noise(x, y, octaves=5, persistence=0.5, lacunarity=2.0):
+#     amplitude = 1.0
+#     frequency = 1.0
+#     noise_val = 0.0
+#
+#     for i in range(octaves):
+#         octave_val = val_noise(x * frequency, y * frequency)[0]
+#         noise_val += octave_val * amplitude
+#         amplitude *= persistence
+#         frequency *= lacunarity
+#
+#     return noise_val, 0.8, 1.0
+#
+# def func(x, y, octaves=1, lacunarity=2, persistence=0.5):
+#     frequency = 1
+#     amplitude = 1
+#     total = 0
+#     for i in range(octaves):
+#         total += val_noise(x * frequency, y * frequency)[0] * amplitude
+#         frequency *= lacunarity
+#         amplitude *= persistence
+#     return total, 0.8, 1
+
+def func(x, y):
+    cloud_noise = 1
+    for octave in range(8):
+        scale = 20 ** octave
+        cloud_noise += val_noise(x * scale, y * scale)[0] * (1 / scale)
+    cloud_noise = cloud_noise * 0.5 + 0.5  # приводим значения к диапазону [0, 1]
+
+    sky_color = (0.6, 0.8, 1.0)
+    horizon_color = (0.0, 0.2, 0.4)
+    sky_y = y * 3 - 1  # приводим y к диапазону [-1, 1]
+    sky_color = tuple((a * (1 - sky_y) + b * (sky_y + 1)) / 2 for a, b in zip(sky_color, horizon_color))
+
+    sky_noise = val_noise(x * 2, y * 0.2)[0] * 0.1  # добавляем небольшой шум к цвету неба
+
+    # комбинируем цвет неба и облаков
+    color = tuple((c * (1 - cloud_noise) + sky_color[i] * cloud_noise) * (1 - sky_noise) for i, c in enumerate(sky_color))
+
+    return color
+
+
+label = tk.Label()
+img = tk.PhotoImage(data=pyshader(func, 256, 256)).zoom(2, 2)
+label.pack()
+label.config(image=img)
+tk.mainloop()
--- file.py
+++ file.py
@@ -1,7 +1,73 @@
-def main(z, x, y):
-    a = ((3*z)**2 + (x**2 - 80*y - 69)**4)
-    b = ((z**3-52*y-x**2)**5)
-    c = (abs(z))**7 + 43*(x**2 + 60 + 49*y)**2
-    return (a/b) + c
+import math
+import tkinter as tk
 
-print(main(1,2,3))
+
+def pyshader(func, w, h):
+    scr = bytearray((0, 0, 0) * w * h)
+    for y in range(h):
+        for x in range(w):
+            p = (w * y + x) * 3
+            scr[p:p + 3] = [max(min(int(c * 255), 255), 0)
+                            for c in func(x / w, y / h)]
+    return bytes('P6\n%d %d\n255\n' % (w, h), 'ascii') + scr
+
+
+# Ваш код здесь:
+import hashlib
+
+def noise_gradient(x, y):
+    """Градиентный шум."""
+    def fade(t):
+        return t * t * t * (t * (t * 6 - 15) + 10)
+
+    def lerp(t, a, b):
+        return a + t * (b - a)
+
+    def grad(hash, x, y):
+        h = hash & 3
+        if h == 0:
+            return x + y
+        elif h == 1:
+            return -x + y
+        elif h == 2:
+            return x - y
+        else:
+            return -x - y
+
+    xi = int(x) & 255
+    yi = int(y) & 255
+    xf = x - int(x)
+    yf = y - int(y)
+    u = fade(xf)
+    v = fade(yf)
+    a = hash_table[hash_table[xi] + yi]
+    b = hash_table[hash_table[xi + 1] + yi]
+    c = hash_table[hash_table[xi] + yi + 1]
+    d = hash_table[hash_table[xi + 1] + yi + 1]
+    x1 = lerp(u, grad(a, xf, yf), grad(b, xf - 1, yf))
+    x2 = lerp(u, grad(c, xf, yf - 1), grad(d, xf - 1, yf - 1))
+    return (lerp(v, x1, x2) + 1) / 2
+
+def fractal_noise(x, y, octaves, persistence):
+    """Фрактальный шум на основе градиентного шума."""
+    amplitude = 1
+    frequency = 1
+    noise_value = 0
+    for i in range(octaves):
+        noise_value += noise_gradient(x * frequency, y * frequency) * amplitude
+        amplitude *= persistence
+        frequency *= 2
+    return noise_value
+
+def func(x, y):
+    """Функция для создания облаков на основе фрактального шума."""
+    value = fractal_noise(x, y, octaves=4, persistence=0.5)
+    value = math.pow(value, 3)
+    return (value, value, value)
+
+
+label = tk.Label()
+img = tk.PhotoImage(data=pyshader(func, 256, 256)).zoom(2, 2)
+label.pack()
+label.config(image=img)
+tk.mainloop()
